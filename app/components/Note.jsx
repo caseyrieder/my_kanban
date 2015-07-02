ece4f9eb22aad8835ec10e6938b0a9bcae30b116
Casey Rieder <casey.rieder@gmail.com>
2015-07-02 10:11:19 -0400

    Added note actions, Notes component, and Flowcheck

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
 
 export default class Note extends React.Component {
-  render() {
-    return <div>Learn Whatever</div>;
-  }
+	constructor(props: {
+		value: string;
+		onEdit: Function;
+	}) {
+		super(props);
+
+		this.state = {
+			edited: false,
+		};
+	}
+	render() {
+		// eslint-disable no-unused-vars
+		const {value, onEdit, ...props} = this.props;
+		// eslint-enable no-unused-vars
+		var edited = this.state.edited;
+
+		return (
+			<div {...props}>{
+				edited
+				? <input type='text'
+					defaultValue={value}
+					onBlur={this.finishedEdit.bind(this)}
+					onKeyPress={this.checkEnter.bind(this)}/>
+				: <div onClick={this.edit.bind(this)}>{value}</div>
+			}</div>
+		);
+	}
+	edit() {
+		this.setState({
+			edited: true,
+		});
+	}
+	checkEnter(e) {
+		if(e.key === 'Enter') {
+			this.finishedEdit(e);
+		}
+	}
+	finishedEdit(e) {
+		this.props.onEdit(e.target.value);
+
+		this.setState({
+			edited: false,
+		});
+	}
 }
